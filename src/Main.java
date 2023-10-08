f5ec5470741607054fedb4266575170549892999
Andrey.Skripay <lordktan@gmail.com>
2023-10-08 18:11:18 +0500

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,14 @@ public class Main {
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(stringArray));
 
+        // Задача 3
+        int [] intA1 = new int[] {3,2,1};
+        double[] numbers1 = {9.986, 7.654,1.57};
+        String[] stringArray1 = new String[]{"orange", "banana", "apple"};
+
+        System.out.println(Arrays.toString(intA1));
+        System.out.println(Arrays.toString(numbers1));
+        System.out.println(Arrays.toString(stringArray1));
+
     }
 }
