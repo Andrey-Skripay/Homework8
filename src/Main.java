db2ba17398bada9fdd90401a30eed42ccf02d709
Andrey.Skripay <lordktan@gmail.com>
2023-10-08 18:06:57 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
         // Задача 1
 
         int [] intA = new int[] {1,2,3};
         double[] numbers = {1.57, 7.654, 9.986};
         String[] stringArray = new String[]{"apple", "banana", "orange"};
+
+        // Задача 2
+        System.out.println(Arrays.toString(intA));
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(stringArray));
 
     }
 }
